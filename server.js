06c78233ed0b8be913676092b7167868b2db6d2f
Stebbi <stefodd@yahoo.com>
2022-04-24 15:42:07 -0500

    added get routes and installed nanoid

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+import { nanoid } from 'nanoid';
+model.id = nanoid();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+const notes = require('./db/db.json');
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+app.post('/api/notes', (req, res) => {
+
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
