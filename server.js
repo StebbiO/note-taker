ddef7772d4a55f86353ca6d0027a4454cf4500a1
Stebbi <stefodd@yahoo.com>
2022-04-24 18:51:47 -0500

    post route writes to db file

--- server.js
+++ server.js
@@ -3,20 +3,19 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
-import { nanoid } from 'nanoid';
-model.id = nanoid();
+const { nanoid } = require('nanoid');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 const notes = require('./db/db.json');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -25,8 +24,19 @@ app.get('/api/notes', (req, res) => {
 
 
 app.post('/api/notes', (req, res) => {
+    let jsonFile = path.join(__dirname, 'db/db.json');
+    let newNote = req.body;
+    newNote.id = nanoid();
 
-})
+    notes.push(newNote);
+
+    fs.writeFile(jsonFile, JSON.stringify(notes), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    res.json(newNote);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
