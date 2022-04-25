3cba54e1580555acfe224a187a6f5ee49b2cc1d6
Stebbi <stefodd@yahoo.com>
2022-04-24 21:37:21 -0500

    added notes/readme, attempted delete function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+For this assignment, the task was to build back end routes for a note taking app that allows users to enter a title and text for a note, and is then saved as part of a JSON file. The contents of the database are presented on the left-hand column, and clicking them brings up the original note.
+
+## Installation
+To install the required dependecies type the following command: npm install
+
+![screenshot of landing page](./public/assets/images/screenshot-landing.jpg)
+![screenshot of notes page](./public/assets/images/screenshot-notes.jpg)
--- server.js
+++ server.js
@@ -3,25 +3,32 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+// node package that generates random ID
 const { nanoid } = require('nanoid');
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 const notes = require('./db/db.json');
 
+// route that 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// route that populates the page with notes notes from the db
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// routes to the landing page if the url is anything but api/notes
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// route to create new note
 app.post('/api/notes', (req, res) => {
     let jsonFile = path.join(__dirname, 'db/db.json');
     let newNote = req.body;
@@ -37,23 +44,34 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
-app.delete('api/notes/:id', (req, res) => {
-    let jsonFile = path.join(__dirname, 'db/db.json');
-
-    for (let i = 0; i < notes.length; i++) {
-        if (notes[i].id === req.params.id) {
-            notes.splice(i, 1);
-            break;
-        }
-    }
-
-    fs.writeFile(jsonFile, JSON.stringify(notes), (err) => {
-        if (err) {
-            throw err;
-        }
-    });
-    res.json(notes);
-});
+// app.delete('api/notes/:id', (req, res) => {
+//     const requestID = req.params.id;
+//     let targetInd;
+    
+//     fs.readFile('db/db.json', (err, data) => {
+//         if (err) throw err;
+//         let notesData = JSON.parse(data);
+    
+//         let targetNote = notesData.filter((note) => {
+//         return note.id === requestID;
+//         })[0];
+    
+//         notes.forEach((item, index) => {
+//         if(item.id === targetNote.id) {
+//             targetInd = index;
+//         }
+//         });
+    
+//         notes.splice(target, 1);
+    
+//         newData = JSON.stringify(notes);
+    
+//         fs.writeFile('db/db.json', newData, (err) => {
+//         if (err) throw err;
+//         });
+//         res.json(notes);
+//     });
+// });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
